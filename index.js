323d52750119f066aee56e52532b03856aa0c25b
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2023-11-04 19:33:55 -0500

    uPdate

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // javascript
+
+const addBtn = document.getElementById('add-btn')
+const deleteBtn = document.getElementById('delete-btn')
+const choresListDisplay = document.getElementById('chores-list-display')
+const inputEl = document.getElementById('input-el')
+
+let myChores = []
+let chorseFromLocalStorage = JSON.parse(localStorage.getItem('myChores'))
+
+function renderChores() {
+    let listItems = ''
+    for (let i = 0; i < myChores.length; i++) {
+        listItems += `<li>${myChores[i]}</li>`
+    }
+    choresListDisplay.innerHTML = listItems
+}
+renderChores()
+
+function addChoreToList() {
+    myChores.push(inputEl.value)
+    inputEl.value = ''
+
+    localStorage.setItem('myChores', JSON.stringify(myChores))
+}
+
+function clearChoreList() {
+    localStorage.clear('choresList')
+    myChores = []
+    choresListDisplay.innerHTML = ``
+}
+
+
+
+addBtn.addEventListener('click', function() {
+    addChoreToList()
+    renderChores()
+})
+deleteBtn.addEventListener('click', clearChoreList)
+
+if (chorseFromLocalStorage) {
+    myChores = chorseFromLocalStorage
+    renderChores()
+}
